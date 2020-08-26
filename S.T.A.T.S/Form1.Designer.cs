b8bed8145bc677e4d7c2fb849ee76db24e7f4c9d
osinmv <osinmv@gmail.com>
2020-08-25 21:35:04 -0400

    Initial Commit
    Start of the App. A lot of mess and a lot of usefull things not done.

---
+++ S.T.A.T.S/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace S.T.A.T.S
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label2 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.condensedViewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label5 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.SeaShell;
+            this.label2.Location = new System.Drawing.Point(25, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Подключение";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bg_worker);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.SeaShell;
+            this.label1.Location = new System.Drawing.Point(108, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.SeaShell;
+            this.label3.Location = new System.Drawing.Point(26, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ContextMenuStrip = this.contextMenuStrip1;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.SeaShell;
+            this.label4.Location = new System.Drawing.Point(23, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(272, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "S.T.A.L.K.E.R S.T.A.T.S ";
+            this.label4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label4_MouseDown);
+            this.label4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label4_MouseMove);
+            this.label4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label4_MouseUp);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.condensedViewToolStripMenuItem,
+            this.exitToolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(132, 70);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.exitToolStripMenuItem.Text = "Full View";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.fullToolStripMenuItem_Click);
+            // 
+            // condensedViewToolStripMenuItem
+            // 
+            this.condensedViewToolStripMenuItem.Name = "condensedViewToolStripMenuItem";
+            this.condensedViewToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.condensedViewToolStripMenuItem.Text = "Small View";
+            this.condensedViewToolStripMenuItem.Click += new System.EventHandler(this.smallViewToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(131, 22);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Black;
+            this.chart1.BorderlineColor = System.Drawing.Color.Black;
+            chartArea1.BackColor = System.Drawing.Color.Black;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.BackColor = System.Drawing.Color.Black;
+            legend1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            legend1.ForeColor = System.Drawing.Color.SeaShell;
+            legend1.IsTextAutoFit = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 146);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Color = System.Drawing.Color.SlateBlue;
+            series1.Legend = "Legend1";
+            series1.MarkerSize = 10;
+            series1.Name = "Speed";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(299, 202);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = " ";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.SeaShell;
+            this.label5.Location = new System.Drawing.Point(220, 190);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Av Speed";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(323, 360);
+            this.ControlBox = false;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.MaximumSize = new System.Drawing.Size(339, 376);
+            this.MinimumSize = new System.Drawing.Size(339, 150);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem condensedViewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ S.T.A.T.S/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using Memory;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace S.T.A.T.S
+{
+    public partial class Form1 : Form
+    {
+        public Mem m = new Mem();
+        public bool ProcOpen = false;
+        public float x, y, z;
+        public float prev_x, prev_y, prev_z;
+        public string pos;
+        public bool loading;
+        public bool restart;
+        public Point m_location;
+        public bool down = false;
+        public System.Windows.Forms.DataVisualization.Charting.Series series1;
+        double speed;
+        int cycle;
+        double total;
+        int over;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            clear_chart();
+            cycle = 0;
+            x = 0;
+            y = 0;
+            z = 0;
+            total = 0.0;
+            over = 0;
+
+        }
+        public void clear_chart()
+        {
+            chart1.Series.Clear();
+            series1 = new System.Windows.Forms.DataVisualization.Charting.Series
+            {
+                Name = "Speed",
+                Color = System.Drawing.Color.Purple,
+                BorderWidth = 5,
+                IsVisibleInLegend = true,
+                IsXValueIndexed = true,
+                ChartType = SeriesChartType.FastLine
+            };
+            chart1.Series.Add(series1);
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bg_worker(object sender, DoWorkEventArgs e)
+        {
+
+            ProcOpen = m.OpenProcess("XR_3DA");
+            if (ProcOpen)
+            {
+                prev_x = x;
+                prev_y = y;
+                prev_z = z;
+                x = m.ReadFloat("base+10BE94");
+                y = m.ReadFloat("base+10BE98");
+                z = m.ReadFloat("base+10BE9C");
+                pos = m.ReadString("xrCore.dll+000BF368,4,0,40,8,10,48,4");
+                cycle++;
+            }
+            Thread.Sleep(1000);
+            backgroundWorker1.ReportProgress(1);
+
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (ProcOpen)
+            {
+                label1.Text = "Connected";
+                speed = Math.Sqrt(Math.Pow(prev_x - x, 2) + Math.Pow(prev_y - y, 2) + Math.Pow(prev_z - z, 2));
+                label3.Text = "x = " + x.ToString() + " y = " + y.ToString() + " z = " + z.ToString() + "place " + pos + " speed " + Math.Round(speed, 2).ToString();
+                if (x == 0 && y == 0 && z == 0)
+                {
+                    clear_chart();
+                }
+                if (speed < 50.0)
+                {
+                    series1.Points.AddXY(cycle, speed);
+                    total += speed;
+                    over++;
+                    label5.Text = "av speed:" + (total / over).ToString();
+                }
+            }
+            else
+            {
+                label1.Text = "Waiting";
+            }
+        }
+
+        private void label4_MouseDown(object sender, MouseEventArgs e)
+        {
+            m_location = e.Location;
+            if (e.Button == MouseButtons.Left)
+            {
+                down = true;
+            }
+        }
+
+        private void label4_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (down)
+            {
+                this.Location = new Point(e.X + this.Location.X - m_location.X, e.Y + this.Location.Y - m_location.Y);
+            }
+        }
+
+
+        private void smallViewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            chart1.Visible = false;
+            this.Height = 150;
+        }
+        private void fullToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            chart1.Visible = true;
+            this.Height = 376;
+        }
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label4_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                down = false;
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+    }
+}
